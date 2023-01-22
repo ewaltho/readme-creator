03c6e7c37819d9fcfe5b629ad5a1f6190464ad6e
Emma Waltho <ejwaltho@gmail.com>
2023-01-22 13:42:53 -0800

    added starter code files

---
+++ index.js
@@ -0,0 +1,12 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquire = require('inquirer')
+
+// TODO: Create an array of questions for user input
+
+
+// TODO: Create a function to write README file
+
+// TODO: Create a function to initialize app
+
+// Function call to initialize app
